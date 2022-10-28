770a05f0f10d6d50f82f33ae92def26d979406ae
vinney-mo <vinneymotanya@gmail.com>
2022-10-28 09:43:35 +0300

    Set DOB Max date to today's date

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,7 @@
+const dob = document.getElementById("dob");
+let currentDate = new Date().toJSON().slice(0, 10);
+dob.max = currentDate;
+
 const form = document.getElementById("gakan");
 
 form.addEventListener("submit", (event) => {
