f868f343929109508a4f23528ea407bb72083085
vinney-mo <vinneymotanya@gmail.com>
2022-10-28 13:17:22 +0300

    Validated Gender selection input

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,8 @@
+//Set max date to today for the DoB input
 let currentDate = new Date().toJSON().slice(0, 10);
 document.getElementById("dob").max = currentDate;
 
+//Akan Name Objects
 const maleAkans = {
   Sunday: "Kwasi",
   Monday: "Kwadwo",
@@ -20,50 +22,79 @@ const femaleAkans = {
   Saturday: "Amma",
 };
 
+//Error display for the iput fields
+const dobRrr = document.getElementById("dob-err");
+const genderERrr = document.getElementById("gender-err");
+
+//Event Listener for the close button for generated Akan names
 const close = document.getElementById("close");
 close.addEventListener("click", (event) => {
   document.getElementById("myAkan").style["display"] = "none";
   document.getElementById("myAkanDetails").innerHTML = "";
 });
 
+//Event listener to clear all
 const clear = document.getElementById("clear");
 clear.addEventListener("click", (event) => {
+  dob.style["border"] = "1px solid #ced4da";
+  dobRrr.style["display"] = "none";
+  dobRrr.innerHTML = "";
+  genderERrr.style["display"] = "none";
+  genderERrr.innerHTML = "";
   document.getElementById("gakan").reset();
   document.getElementById("myAkan").style["display"] = "none";
   document.getElementById("myAkanDetails").innerHTML = "";
 });
 
+//Even listener for submitting form
 const form = document.getElementById("gakan");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  let err = 0;
   const dob = document.getElementById("dob");
-  const err = document.getElementById("dob-err");
+
+  //Validate DoB input
   if (dob.value == "") {
     dob.style["border"] = "1px solid red";
-    err.style["display"] = "block";
-    err.innerHTML = "Please input your Date Of Birth";
-    return false;
+    dobRrr.style["display"] = "block";
+    dobRrr.innerHTML = "Please input your Date Of Birth";
+    err++;
   } else {
     var ToDate = new Date();
     if (new Date(dob.value).getTime() > ToDate.getTime()) {
       dob.style["border"] = "1px solid red";
-      err.style["display"] = "block";
-      err.innerHTML =
+      dobRrr.style["display"] = "block";
+      dobRrr.innerHTML =
         "Invalid Date Of Birth. Input a date less than or equal to today";
-      return false;
+      err++;
     } else {
       dob.style["border"] = "1px solid #ced4da";
-      err.style["display"] = "none";
-      err.innerHTML = "";
+      dobRrr.style["display"] = "none";
+      dobRrr.innerHTML = "";
     }
   }
 
+  //Validate Gender selection
+  if (!document.querySelector('input[name="gender"]:checked')) {
+    genderERrr.style["display"] = "block";
+    genderERrr.innerHTML = "Please select gender";
+    err++;
+  } else {
+    genderERrr.style["display"] = "none";
+    genderERrr.innerHTML = "";
+  }
+
+  //If errors resturn falsse
+  if (err > 0) return false;
+
+  //If no errors proced to genate Akan names
   document.getElementById("submit").disabled = true;
   document.getElementById("submit").innerHTML = "Generating . . .";
 
   const birthday = dob.value;
   const gender = document.querySelector('input[name="gender"]:checked').value;
 
+  //Get day of the week from DoB
   const dayOfWeekName = new Date(birthday).toLocaleString("default", {
     weekday: "long",
   });
@@ -74,6 +105,7 @@ form.addEventListener("submit", (event) => {
     myAkanName = femaleAkans[dayOfWeekName];
   }
 
+  //Reset form and diaplay info
   document.getElementById("gakan").reset();
   document.getElementById("submit").disabled = false;
   document.getElementById("submit").innerHTML = "Generate name";
